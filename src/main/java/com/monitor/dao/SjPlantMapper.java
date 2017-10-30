d7ce162d7ff5fe467a9f95064d0483aed0f7cb23
yuanyoumin <15270817790@163.com>
2017-10-30 17:39:08 +0800

    111

--- src/main/java/com/monitor/dao/SjPlantMapper.java
+++ src/main/java/com/monitor/dao/SjPlantMapper.java
@@ -8,7 +8,7 @@ import com.monitor.pojo.*;
 
 public interface SjPlantMapper {
     int deleteByPrimaryKey(Integer id);
-
+//把数据插入到plant里
     int insert(SjPlantinfo record);
 
     int insertSelective(SjPlantinfo record);
@@ -18,7 +18,8 @@ public interface SjPlantMapper {
     int updateByPrimaryKeySelective(SjPlantinfo record);
 
     int updateByPrimaryKey(SjPlantinfo record);
+  //查询plant的所有数据
     List<SjPlantinfo> selectAll();
-    //查询条数
+    //查询plant条数
     List<SjPlantinfo> selectTop(@Param("num1")int sum1,@Param("num2")int num2);
 }
